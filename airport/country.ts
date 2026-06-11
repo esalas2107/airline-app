85f07fdf1c4cb018bb9ffb31ab0d6578779e47d6
Esteban Salas <estebanandressalasalfaro@gmail.com>
2026-06-11 16:17:30 -0600

    chore(airport): add country class

---
+++ airport/country.ts
@@ -0,0 +1,30 @@
+class Country {
+    private _countryId: number;
+    private _countryName: string;
+
+    constructor(countryId: number, countryName: string) {
+        this._countryId = countryId;
+        this._countryName = countryName;
+    }
+
+    get countryId(): number {
+        return this._countryId;
+    }
+
+    get countryName(): string {
+        return this._countryName;
+    }
+
+    toString(): string {
+        return `
+        *** COUNTRY INFORMATION ***
+        ID: ${this.countryId}
+        NAME: ${this.countryName}
+        `;
+    }
+
+}
+
+const country1 = new Country(1, "Senegal");
+
+console.log(country1.toString());
